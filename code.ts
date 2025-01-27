5474a5374093c90876aee344da43594c35e4591f
akash100a <arakash968786@gmail.com>
2025-01-27 20:10:29 +0600

    update

--- code.ts
+++ code.ts
@@ -12,10 +12,15 @@ figma.showUI(__html__);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
-figma.ui.onmessage =  (msg: {type: string, count: number}) => {
+
+figma.ui.onmessage = (msg: {
+  type: string;
+  count: number;
+  opacityPersentence: number;
+}) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-shapes') {
+  if (msg.type === "create-shapes") {
     // This plugin creates rectangles on the screen.
     const numberOfRectangles = msg.count;
 
@@ -23,15 +28,40 @@ figma.ui.onmessage =  (msg: {type: string, count: number}) => {
     for (let i = 0; i < numberOfRectangles; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
+      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
+  } else if (msg.type === "opacityChange") {
+    const zz = msg.opacityPersentence;
+    for (const node of figma.currentPage.selection) {
+      if ("opacity" in node) {
+        node.opacity = zz / 100;
+      }
+    }
+    console.log("Akash");
+    // console.log(zz);
+  } else if (msg.type === "get-Code") {
+    // const zz = msg.opacityPersentence;
+    for (const node of figma.currentPage.selection) {
+      let p = node.height;
+      console.log(p);
+      
+      if ("opacity" in node) {
+        const op = node.opacity * 100;
+        const opval = op.toFixed(0);
+        // Send a message to the UI
+        figma.ui.postMessage({
+          type: "code",
+          message: [
+            `opacity-${opval}`,
+          ],
+        });
+      }
+    }
+  } else {
+    figma.closePlugin();
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
 };
